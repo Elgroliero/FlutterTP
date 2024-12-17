34720314d4acaa03a1f10e589e14cf84d586d83d
stropee <stropee@campus-eni.fr>
2024-12-17 16:29:52 +0100

    TP création widget, vues , tweeter

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tp_rcda32/content-body.dart';
+import 'package:flutter_tp_rcda32/footer.dart';
+import 'package:flutter_tp_rcda32/header.dart';
+
+void main() {
+  runApp(const TwitterApp());
+}
+
+class TwitterApp extends StatelessWidget {
+  const TwitterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Twitter',
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.pinkAccent)),
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blueAccent.shade400,
+            title: Center(
+                child: Text('Tweety !',
+                    style: TextStyle(fontSize: 24, color: Colors.white))),
+          ),
+          body: Column(
+            children: [Header(), Expanded(child: ContentBody()), Footer()],
+          ),
+        ));
+  }
+}
